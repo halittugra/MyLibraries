446b24497f6ad1275b9340d5292a0980abf2498e
halittugra <48262224+halittugra@users.noreply.github.com>
2020-01-21 14:14:58 +0300

    Update comh.h

--- comh.h
+++ comh.h
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int lo;
 
-void printArray(lo arr[],lo n){
-    for(lo i = 0;i < n;i++) cout << arr[i];
+lo LinearSearch(lo arr[],lo n,lo s){
+    for(lo i = 0;i < n;i++){
+        if(arr[i] == s) return i;
+    }
+    return -1;
+}
+
+lo LinearSearch(lo arr[],lo s,lo l,lo r){
+    for(lo i = l;i < r;i++){
+        if(arr[i] == s) return i;
+    }
+    return -1;
+}
+
+lo LinearSearch(vector<lo> arr,lo n,lo s){
+    for(lo i = 0;i < n;i++){
+        if(arr[i] == s) return i;
+    }
+    return -1;
+}
+
+lo LinearSearch(vector<lo> arr,lo s,lo l,lo r){
+    for(lo i = l;i < r;i++){
+        if(arr[i] == s) return i;
+    }
+    return -1;
 }
 
